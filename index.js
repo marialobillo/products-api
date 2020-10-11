c40e50fa434975c36554717c3ff83f97ad838e70
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-10-11 13:51:02 +0200

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const express = require('express');
 
 const app = express();
 
+// Database in memory
+const products = [
+    { id: '1234', title: 't-shirt', price: 40, coin: 'USD'},
+    { id: '1235', title: 'jeans', price: 60, coin: 'USD'},
+    { id: '1236', title: 'shoes', price: 90, coin: 'USD'}
+];
+
+app.route('/products')
+    .get((req, res) => {
+        res.json(products)
+    })
+
+app.get('/products/:id', (req, res) => {
+    for(let product of products){
+        if(product.id == req.params.id){
+            res.json(product);
+            return;
+        }
+    }
+    res.status(404).send(`The product with id : [${req.params.id}] is not found.`);
+})
+
 app.get('/', (req, res) => {
     res.send('API of shop online')
 })
