afecfd4093c362f993a89fb35be8b809330216fe
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-10-14 17:37:33 +0200

    modularize database

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const _ = require('underscore');
+const products = require('./database').products;
 
 const app = express();
-
-// Database in memory
-const products = [
-    { id: '1234', title: 't-shirt', price: 40, coin: 'USD'},
-    { id: '1235', title: 'jeans', price: 60, coin: 'USD'},
-    { id: '1236', title: 'shoes', price: 90, coin: 'USD'}
-];
 
 // Middleware
 app.use(express.json());
