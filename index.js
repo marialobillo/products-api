4b64f05c09d66a8e49b5d453c6505fce8ca4f03b
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-10-15 19:57:25 +0200

    module for logs

--- index.js
+++ index.js
@@ -2,39 +2,8 @@ const express = require('express');
 const app = express();
 const productsRouter = require('./api/resources/products/products.routes');
 const winston = require('winston');
+const logger = require('./utils/logger');
 
-const includeTimestamp = winston.format((info) => {
-    info.message = `${new Date().toISOString()} ${info.message}`
-    return info
-});
-
-const logger = winston.createLogger({
-    transports: [
-        new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
-            )
-        }),
-        new winston.transports.File({
-            level: 'info',
-            handleExceptions: true, 
-            format: winston.format.combine(
-                includeTimestamp(),
-                winston.format.simple()
-            ),
-            maxsize: 5120000, // 5 Mb
-            maxFiles: 5,
-            filename: `${__dirname}/application-logs.log`
-        })
-    ]
-});
-
-
-// Logs
-logger.info('This is a info message');
 
 // Middleware
 app.use(express.json());
@@ -47,5 +16,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3002, () => {
-    console.log('Server running on port 3002');
+    logger.info('Server running on port 3002');
 })
