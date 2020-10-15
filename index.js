89e847f2b069b8eb4f4ce20761a21e089741af52
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-10-15 18:02:13 +0200

    adding logs

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const express = require('express');
-
 const app = express();
 const productsRouter = require('./api/resources/products/products.routes');
+const winston = require('winston');
+
+const includeTimestamp = winston.format((info) => {
+    info.message = `${new Date().toISOString()} ${info.message}`
+    return info
+});
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            level: 'info',
+            handleExceptions: true, 
+            format: winston.format.combine(
+                includeTimestamp(),
+                winston.format.simple()
+            ),
+            maxsize: 5120000, // 5 Mb
+            maxFiles: 5,
+            filename: `${__dirname}/application-logs.log`
+        })
+    ]
+});
+
+
+// Logs
+logger.info('This is a info message');
 
 // Middleware
 app.use(express.json());
