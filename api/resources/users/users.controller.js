a7ff128132b787531b7991cd3db3ee6c887ff842
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-11-19 12:20:24 +0100

    adding functionality to the controller for users: create User and checking if user exists

--- api/resources/users/users.controller.js
+++ api/resources/users/users.controller.js
@@ -4,6 +4,27 @@ function getUsers(){
     return User.find({})
 }
 
+function createUser(user, hashedPassword){
+    return new User({
+        ...user, 
+        password: hashedPassword
+    }).save()
+}
+
+function userExists(username, email){
+    return new Promise((resolve, reject) => {
+        User.find().or([ {'username': username}, {'email': email} ])
+            .then(users => {
+                resolve(users.length > 0)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
+
 module.exports = {
-    getUsers
+    getUsers,
+    createUser, 
+    userExists
 }
