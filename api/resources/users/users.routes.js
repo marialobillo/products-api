00ebfd99fac7598fa32f7ba1df06d097f4d6820e
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-11-20 19:15:37 +0100

    post route for create new user

--- api/resources/users/users.routes.js
+++ api/resources/users/users.routes.js
@@ -27,34 +27,37 @@ usersRouter.get('/', (req, res) => {
 usersRouter.post('/', validateUser, (req, res) => {
     let newUser = req.body;
 
-    let index = _.findIndex(users, user => {
-        return user.username === newUser.username || user.email === newUser.email;
-    })
-
-    if(index !== -1){
-        // Conflict
-        logger.info('Email or username already exist on database.');
-        res.status(409).send('The email or username already exist.');
-        return;
-    }
-
-    bcrypt.hash(newUser.password, 10, (error, hashedPassword) => {
-        if(error){
-            // Internal Server Error
-            logger.error('Error getting the hash for password', error);
-            res.status(500).send("There was an error creating the user");
-            return;
+    userController,userExists(newUser.username, newUser.email)
+    .then(userExists => {
+        if(userExists){
+            logger.warn(`Email ${newUser.email} or username ${newUser.username} already exists.`)
+            res.status(409).send('The email or username already associate to an account')
+            return
         }
 
-        users.push({
-            username: newUser.username,
-            email: newUser.email,
-            password: hashedPassword,
-            id: uuidv4()
-        });
+        bcrypt.hash(newUser.password, 10, (error, hashedPassword) => {
+            if(error){
+                logger.error('Error trying to get password hash', err)
+                res.status(500).send('Error on creating the User')
+                return
+            }
 
-        res.status(201).send('User created successfully');
-    });
+            userController.createUser(newUser, hashedPassword)
+                .then(newUser => {
+                    res.status(201).send('User create successfully')
+                })
+                .catch(error => {
+                    logger.error('Error trying to create a new User', error)
+                    res.status(500).send('Error trying to create a new User')
+                })
+        })
+    })
+    .catch(error => {
+        logger.error(`Error on User verification ${newUser.username}`)
+        res.status(500).send('Error when we try to create a new User')
+    })
+
+  
 });
 
 usersRouter.post('/login', orderLoginValidation, (req, res) => {
