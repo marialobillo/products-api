099ab7a134c355e3e6f426f5ec74df7c42d9b857
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-11-24 11:49:26 +0100

    connecting auth with mongodb

--- api/libs/auth.js
+++ api/libs/auth.js
@@ -1,27 +1,36 @@
 const _ = require('underscore');
-const logger = require('./../../utils/logger');
-const users = require('./../../database').users;
 const bcrypt = require('bcrypt');
 const passportJWT = require('passport-jwt');
-const config = require('./../../config');
 
+const config = require('./../../config');
+const logger = require('./../../utils/logger');
+const userController = require('../resources/users/users.controller');
+
+// Authorization : bearer token
 let jwtOptions = {
     secretOrKey: config.jwt.secret,
     jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken()
 }
 
 module.exports = new passportJWT.Strategy(jwtOptions, (jwtPayload, next) => {
-    let index = _.findIndex(users, user => user.id === jwtPayload.id)
 
-    if(index === -1){
-        logger.info(`JWT token is not valid. User with id ${jwtPayload.id} does not exist.`);
-        next(null, false);
-    } else {
-        logger.info(`User ${users[index].username} got a valid token. Auth OK`);
-        next(null, {
-            username: users[index].username,
-            id: users[index].id
-        });
-    }
+    userController.getUser({ id: jwtPayload.id })
+        .then(user => {
+            if(!user){
+                logger.info(`JWT token is not valid. User with id ${jwtPayload.id} does not exist.`);
+                next(null, false);
+                return;
+            }
+
+            logger.info(`User ${user.username} got a valid token. Auth OK`);
+            next(null, {
+                username: user.username, 
+                id: user.id
+            })
+        })
+        .catch(error => {
+            logger.error('Error trying validating the token', error);
+            next(error);
+        })
 
 })
