a337acd71d98563b10f76b2dd89b20c32181966a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-12-03 12:24:22 +0100

    testing getting all users

--- api/resources/users/users.routes.test.js
+++ api/resources/users/users.routes.test.js
@@ -4,6 +4,23 @@ let User = require('./users.model');
 let app = require('../../../index').app;
 let server = require('../../../index').server;
 
+let dummyUsers = [
+    {
+        username: 'Peter',
+        email: 'peter@mail.com',
+        password: 'hello123'
+    },
+    {
+        username: 'Jon',
+        email: 'jon@mail.com',
+        password: 'hello123'
+    },
+    {
+        username: 'Kim',
+        email: 'kim@mail.com',
+        password: 'hello123'
+    }
+];
 describe('** Users **', () => {
 
     beforeEach((done) => {
@@ -16,7 +33,7 @@ describe('** Users **', () => {
         server.close()
     })
 
-    describe('GET /products', () => {
+    describe('GET /users', () => {
 
         test('If there is not Users, should returns an empty array', (done) => {
             request(app)
@@ -26,6 +43,20 @@ describe('** Users **', () => {
                     expect(res.body).toBeInstanceOf(Array)
                     expect(res.body).toHaveLength(0)
                     done()
+                })
+        })
+
+        test('If there is Users, should return them on an array', (done) => {
+            Promise.all(dummyUsers.map(user => (new User(user)).save()))
+                .then(users => {
+                    request(app)
+                        .get('/users')
+                        .end((error, res) => {
+                            expect(res.status).toBe(200)
+                            expect(res.body).toBeInstanceOf(Array)
+                            expect(res.body).toHaveLength(3)
+                            done()
+                        })
                 })
         })
     })
